031451e7fb0ae853933d924bbe0332118ba795c3
Qihao He <qi.he@maine.edu>
2020-01-21 16:27:20 -0500

    fix merger

--- src/com/luv2code/hibernate/demo/entity/Student.java
+++ src/com/luv2code/hibernate/demo/entity/Student.java
@@ -6,80 +6,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-=======
->>>>>>> parent of e8a77ea... read dates with hibernate
-=======
->>>>>>> parent of e8a77ea... read dates with hibernate
+
 
 @Entity
 @Table(name="student")
 public class Student {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    
-    @Id
-    @Column(name="id")
-    private int id;
-    
-    @Column(name="first_name")
-    private String firstName;
-    
-    @Column(name="last_name")
-    private String lastName;
-    
-    @Column(name="email")
-    private String email;
-    
-    public Student() {
-        
-    }
-
-    public Student( String firstName, String lastName, String email) {
-        
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-=======
-=======
->>>>>>> parent of e8a77ea... read dates with hibernate
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -135,23 +70,14 @@ public class Student {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-<<<<<<< HEAD
->>>>>>> parent of e8a77ea... read dates with hibernate
-=======
->>>>>>> parent of e8a77ea... read dates with hibernate
+
 
 	@Override
 	public String toString() {
 		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
 	}
-<<<<<<< HEAD
-<<<<<<< HEAD
-}
-=======
+
+
 	
 }
->>>>>>> parent of e8a77ea... read dates with hibernate
-=======
-	
-}
->>>>>>> parent of e8a77ea... read dates with hibernate
+
